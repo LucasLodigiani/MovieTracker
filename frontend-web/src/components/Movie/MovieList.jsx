28be7a09e1163ee97c43d2eddce19f22fa49f133
santy188 <tisanberon@gmail.com>
2023-06-22 19:32:31 -0300

    MovieList update

--- frontend-web/src/components/Movie/MovieList.jsx
+++ frontend-web/src/components/Movie/MovieList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { base_url } from '../../utils/Config';
 const MovieList = ({ movies }) => {
   return (
     <>
       {movies.map((movie, index) => (
         <div key={index}>
-          <img src={movie.Poster} alt='movie' />
+          <img src={base_url + '/media/' + movie.imageUrl} alt='movie' />
         </div>
       ))}
     </>
