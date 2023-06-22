739deb543cf1ab8e049466dc618c5dc7f75a9113
santy188 <tisanberon@gmail.com>
2023-06-22 13:56:33 -0300

    Movie main page

--- frontend-web/src/components/Main.jsx
+++ frontend-web/src/components/Main.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Row from './Shared/Row';
+
 
 const Main = () => {
+  const [movie, setMovie] = useState();
+  const genreWarMovies = movie.filter(movie => movie.Genre.includes('War'));
+  const genreComedyMovies = movie.filter(movie => movie.Genre.includes('Comedy'));
+  const genreDramaMovies = movie.filter(movie => movie.Genre.includes('Drama'));
+  const genreCrimeMovies = movie.filter(movie => movie.Genre.includes('Crime'));
   return (
-    <div className="">
-      {/* Content of the main section */}
+    
+    <div>
+      <Row movie={genreComedyMovies} Genre='Comedia' />
+      {/* <Row movie={genreWarMovies} Genre='Belica' /> */}
+      <Row movie={genreDramaMovies} Genre='Drama'/>
+      <Row movie={genreCrimeMovies} Genre='Crime'/>
+      
     </div>
-   
   );
 };
 
