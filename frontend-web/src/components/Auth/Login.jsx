68e24b22c7c3db3493a129b1b06c7940578f1a9f
santy188 <tisanberon@gmail.com>
2023-06-30 14:33:33 -0300

    visual changes

--- frontend-web/src/components/Auth/Login.jsx
+++ frontend-web/src/components/Auth/Login.jsx
@@ -15,6 +15,7 @@ const Login = () => {
   const { user, setUser } = useContext(UserContext);
   const [userData, setUserData, isAuthenticated, isInRole, Logout] = useAuthentication();
 
+
   const userNameHandler = (event) => {
     setUsername(event.target.value);
   }
@@ -26,6 +27,7 @@ const Login = () => {
   const handleLogin = async (event) => {
     event.preventDefault();
     await login({ username, password });
+
   }
 
   const handleLogout = () => {
@@ -34,32 +36,31 @@ const Login = () => {
 
   //{`${theme === 'light' ? '' : ''}`}     <Link to="/" >Sign In </Link>
   return (
-    <div className={`flex items-center justify-center min-h-screen ${theme === 'light' ? 'bg-gray-100' : 'gray text-gray-700'}`} >
-      <div className={`relative flex flex-col m-6 space-y-8 ${theme === 'light' ? 'bg-white shadow-2xl' : 'bg-sky-500 shadow-2xl'}  rounded-2xl md:flex-row md:space-y-0'`}>
+    <form className={`flex items-center justify-center min-h-screen ${theme === 'light' ? 'bg-gray-100' : 'gray text-gray-700'}`} >
+      <div className={`relative flex flex-col m-6 space-y-8 ${theme === 'light' ? 'bg-white shadow-2xl' : 'bg-indigo-900 shadow-2xl'}  rounded-2xl md:flex-row md:space-y-0'`}>
         <div className='flex flex-col justify-center p-8 md:p-14'>
-          <label className={`mb-3 text-4xl font-bold ${theme === 'dark' ? 'text' : '' }`}>Bienvenido!</label>
+          <label className={`mb-3 text-4xl font-bold items-center ${theme === 'dark' ? 'text-white' : ''}`}>Bienvenido!</label>
           <div className='py-4'>
-            <label className='mb-2 text-mb'>Usuario</label>
-            <input type='text' onChange={userNameHandler} value={username} className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : ' border-white'} rounded-md `}></input>
+            <label className={`mb-2 text-mb ${theme === 'light' ? 'text-black' : 'text-white'}`} >Usuario</label>
+            <input type='text' onChange={userNameHandler} value={username} className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : 'bg-gray-700 text-white border-sky-700'} rounded-md `}></input>
           </div>
-          <div className='py-8'>
-            <label className='mb-2 text-mb'>Contraseña</label>
-            <input type='password' onChange={passwordHandler} value={password} className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : ''} rounded-md `}></input>
+          <div className='py-4'>
+            <label className={`mb-2 text-mb ${theme === 'light' ? 'text-black' : 'text-white'}`}>Contraseña</label>
+            <input type='password' onChange={passwordHandler} value={password} className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : 'bg-gray-700 text-white border-sky-700'} rounded-md `}></input>
 
           </div>
-          <button onClick={handleLogin} className={`w-full ${theme === 'light' ? 'bg-red-800 text-white hover:bg-gradient-to-r from-red-500 to-rose-700' :''} p-2 rounded-lg mb-6 `}> Sign In</button>
-          {userError && <p>{userError}</p>}
-          
-          <div className={`text-center ${theme === 'light' ? 'text-black' : ''}`}>No tiene una cuenta?
-            <label className='font-bold'> <Link to="/register" className={`${theme === 'light' ? 'hover:text-pink-700' : 'hover:text-cyan-300'}`}>Registrese aqui!</Link></label>
+          <button onClick={handleLogin} className={`w-full ${theme === 'light' ? 'bg-red-800 text-white hover:bg-gradient-to-r from-red-500 to-rose-700' : 'bg-gray-700 text-white hover:bg-gradient-to-r from-cyan-800 to-blue-600'} p-2 rounded-lg mb-6 `}> Sign In</button>
+          {userError && <p className={`${theme === 'light' ? 'text-red-800' : 'text-white'}`}>{userError}</p>}
+
+          <div className={`text-center ${theme === 'light' ? 'text-black' : 'text-white'}`}>No tiene una cuenta?
+            <label className='font-bold'> <Link to="/register" className={`${theme === 'light' ? 'hover:text-pink-700' : 'hover:text-cyan-700'}`}>Registrese aqui!</Link></label>
           </div>
         </div>
-        <div className='relative'>
-        </div>
+
       </div>
 
 
-{/* 
+      {/* 
 
       {jwt && <p>jwt: {jwt}</p>} */}
       {/* {isUserLoading && <p>{isUserLoading}</p>}
@@ -68,8 +69,8 @@ const Login = () => {
 
       <p>UserData: {userData && <span>{userData.name}</span>}</p>
       <button onClick={handleLogout}>Desloguear</button> */}
-     
-    </div>
+
+    </form>
   )
 }
 
--- frontend-web/src/components/Auth/Register.jsx
+++ frontend-web/src/components/Auth/Register.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import backgroundImage from '../../assets/images/movies-background.jpg';
 import useAuth from '../../hooks/useAuth';
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 
 //TO DO: Modularizar todo este componente.
 const Register = () => {
-  //Deconstruir la funcion
+  const { theme } = useContext(ThemeContext)
   const { Register } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
@@ -34,7 +35,7 @@ const Register = () => {
     event.preventDefault();
     setMessage(null);
     setIsLoading(true);
-    const result = await Register({username,email,password});
+    const result = await Register({ username, email, password });
     setIsLoading(false);
     if (result.ok) {
       // Registro exitoso
@@ -48,83 +49,43 @@ const Register = () => {
   };
 
   return (
-    <div
-      className="flex justify-center items-center h-screen"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-      }}
-    >
-      <form className="bg-slate-900 shadow-md px-10 py-8 w-96">
-        <h1 className="text-white text-4xl font-bold mb-2">Crea una cuenta</h1>
-        <div className="mb-8">
-          <label className="text-white text-sm font-bold mb-2" htmlFor="username">
-            Nombre de usuario
-          </label>
-          <input
-            className="border rounded w-full py-3 px-4 text-gray-700 leading-tight"
-            id="username"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-        </div>
-        <div className="mb-8">
-          <label className="text-white text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="border rounded w-full py-3 px-4 text-gray-700 leading-tight"
-            id="email"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-        <div className="mb-8">
-          <label className="text-white text-sm font-bold mb-2" htmlFor="password">
-            Contraseña
-          </label>
-          <input
-            className="border rounded w-full py-3 px-4 text-gray-700 leading-tight"
-            id="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <div className="mb-8">
-          <input className="mr-2" type="checkbox" value={checkbox} onChange={handleCheckboxChange} />
-          <p className="text-white inline-block align-middle">Acepto los términos y condiciones</p>
-        </div>
-        <div className="flex items-center justify-center">
+    <form className={`flex items-center justify-center min-h-screen ${theme === 'light' ? 'bg-gray-100' : 'gray text-gray-700'}`}>
+      <div className={`relative flex flex-col m-6 space-y-8 ${theme === 'light' ? 'bg-white shadow-2xl' : 'bg-indigo-900 shadow-2xl'}  rounded-2xl md:flex-row md:space-y-0'`}>
+        <div className='flex flex-col justify-center p-8 md:p-14'>
+          <label className={`mb-3 mx-20 text-4xl font-bold items-center ${theme === 'dark' ? 'text-white' : ''}`}>Registrate</label>
+          <div className='py-4'>
+            <label className={`mb-2 text-mb ${theme === 'light' ? 'text-black' : 'text-white'}`} htmlFor="username"> Nombre de usuario </label>
+            <input className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : 'bg-gray-700 text-white border-sky-700'} rounded-md `} id="username" type="text" placeholder="Username" value={username} onChange={handleUsernameChange} required />
+          </div>
+          <div className='py-4'>
+            <label className={`mb-2 text-mb ${theme === 'light' ? 'text-black' : 'text-white'}`} htmlFor="email">Email</label>
+            <input className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : 'bg-gray-700 text-white border-sky-700'} rounded-md `} id="email" type="email" placeholder="Email" value={email} onChange={handleEmailChange} required />
+          </div>
+          <div className='py-4'>
+            <label className={`mb-2 text-mb ${theme === 'light' ? 'text-black' : 'text-white'}`} htmlFor="password">Contraseña</label>
+            <input className={`w-full p-2 border ${theme === 'light' ? 'border-gray-700 ' : 'bg-gray-700 text-white border-sky-700'} rounded-md `} id="password" type="password" placeholder="Password" value={password} onChange={handlePasswordChange} required />
+  
+          </div>
+          <div className="flex items-center justify-center py-4">
+           
+          </div>
           {isLoading ? (
-            <button
-              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-              type="submit"
-              disabled
-            >
-              Cargando...
-            </button>
-          ) : (
-            <button
-              className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              type="submit"
-              onClick={HandleRegister}
-            >
-              Registrar
-            </button>
-          )}
+              <button
+                className={`w-full ${theme === 'light' ? 'bg-red-500  hover:bg-red-700 text-dark' : 'bg-blue-500 hover:bg-blue-700 text-white'} font-bold py-2 px-4 rounded`}
+                type="submit"
+                disabled
+              >
+                Cargando...
+              </button>
+            ) : (
+              <button className={`w-full ${theme === 'light' ? 'bg-red-800 text-white hover:bg-gradient-to-r from-red-500 to-rose-700' :'bg-gray-700 text-white hover:bg-gradient-to-r from-cyan-800 to-blue-600'} p-2 rounded-lg mb-6 `}type="submit"onClick={HandleRegister}>Registrar</button>
+
+            )}
+            {message && <p className={`${theme === 'light' ? 'text-red-800' : 'text-blue-200'}`}>{message}</p>}
         </div>
-        {message && <p className='text-center'>{message}</p>}
-      </form>
-    </div>
+      </div>
+    </form>
+
   );
 };
 
